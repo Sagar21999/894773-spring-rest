c528f7592c221fc01a154276f49c8083f2f08ffd
SAGAR <SAGAR@LAPTOP-HABIHCG1>
2021-03-21 16:47:52 +0530

    spring rest handson2

--- spring-learn2/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
+++ spring-learn2/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -14,9 +14,6 @@ public class SpringLearnApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLearnApplication.class, args);
 		
-		/*
-		 * logger.setLevel(Level.FINE); displayCountry(); displayCountries();
-		 */
 	}
 
 	
